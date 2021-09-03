570a8b7795488d4685084c6b3d25dc21c814f5d6
Nikolay Kuznetsov <NI.Kuznetsov@gaz-is.ru>
2021-09-03 16:41:51 +0300

    Refactor code

--- tr_id_module.c
+++ tr_id_module.c
@@ -23,29 +23,59 @@ typedef struct {
     uint64 data[1024];
 } FullTransactionIdSharedState;
 
+// signals variables
 static volatile sig_atomic_t got_sigterm = false;
 static volatile sig_atomic_t got_sighup = false;
 
-static FullTransactionIdSharedState* fti_state = NULL;
-static char* default_log_path = "/home/postgres/log.log";
-static char* log_path = NULL;
-static FILE* log_file = NULL;
-static FullTransactionId tr_id;
+//-----------------------
+// Function definition
+//-----------------------
 
+// Xact callback func
+void FTI_inv_callback(XactEvent event, void* arg);
+
+// logging functions
+void stop_logging(void);
+void start_logging(void);
+
+// Register func
+PG_FUNCTION_INFO_V1(set_new_log);
+PG_FUNCTION_INFO_V1(get_top_tr_id);
+
+// Signal handlers
 static void tim_sigterm_hadler(SIGNAL_ARGS);
 static void tim_sighup_hadler(SIGNAL_ARGS);
 
-// static BackgroundWorkerHandle* handler;
+// Background Worker main func
+void spooler_main(Datum) pg_attribute_noreturn();
+
+// Init Shmem func
+void init_shmem(void);
+
+// Module init func
+void _PG_init(void);
+
+//-------------------
+// Variables
+//-------------------
 //
-int tr_id_module_worker_start(void);
-//
-void FTI_inv_callback(XactEvent event, void* arg);
+static FullTransactionIdSharedState* fti_state = NULL;
+
+static char* log_path = NULL;
+static FILE* log_file = NULL;
+static int spool_limit;
+
+static FullTransactionId tr_id;
+
+//------------------------
+// Start code
+//------------------------
+
 void FTI_inv_callback(XactEvent event, void* arg) {
     if (event != XACT_EVENT_PRE_COMMIT) return;
 
     tr_id = GetTopFullTransactionId();
     LWLockAcquire(&fti_state->lock, LW_EXCLUSIVE);
-    // fti_state->data = tr_id;
 
     fti_state->data[fti_state->num_elem] = U64FromFullTransactionId(tr_id);
     fti_state->num_elem++;
@@ -54,25 +84,18 @@ void FTI_inv_callback(XactEvent event, void* arg) {
 
     elog(LOG, "TransactionId = %lu", U64FromFullTransactionId(tr_id));
 }
-//
-void stop_logging(void);
 void stop_logging(void) {
     fclose(log_file);
 
     elog(LOG, "Logging stopped");
 }
 
-void start_logging(void);
 void start_logging(void) {
-    if (log_path != NULL)
-	log_file = fopen(log_path, "a+");
-    else
-	log_file = fopen(default_log_path, "a+");
+    log_file = fopen(log_path, "a+");
 
     elog(LOG, "Logging started");
 }
 
-PG_FUNCTION_INFO_V1(set_new_log);
 Datum set_new_log(PG_FUNCTION_ARGS) {
     elog(LOG, "Start switching: new path - %s", log_path);
 
@@ -84,10 +107,8 @@ Datum set_new_log(PG_FUNCTION_ARGS) {
     PG_RETURN_VOID();
 }
 
-PG_FUNCTION_INFO_V1(get_top_tr_id);
 Datum get_top_tr_id(PG_FUNCTION_ARGS) { PG_RETURN_UINT64(1); }
 
-void init_shmem(void);
 void init_shmem(void) {
     bool found;
 
@@ -105,11 +126,7 @@ void init_shmem(void) {
     LWLockRegisterTranche(fti_state->lock.tranche, "fri_catch");
 }
 
-void spooler_main(Datum) pg_attribute_noreturn();
 void spooler_main(Datum main_arg) {
-    //
-    // ereport(DEBUG1, (errmsg("spooler_main started: Log path: qwe")));
-
     uint64* buf;
     size_t size;
     bool need_clean = false;
@@ -119,17 +136,10 @@ void spooler_main(Datum main_arg) {
     pqsignal(SIGUSR1, procsignal_sigusr1_handler);
     BackgroundWorkerUnblockSignals();
 
-    elog(LOG, "BGWorker started");
-
-    // init_shmem();
+    elog(LOG, "BGWorker started: spool_limit = %d", spool_limit);
 
     while (!got_sigterm) {
-	// tr_id = GetTopFullTransactionId();
-
-	// if (c % 10000 != 0) continue;
-
-	// tr.value = fti_state->data.value;
-	if (fti_state->num_elem > 10) {
+	if (fti_state->num_elem > spool_limit) {
 	    need_clean = true;
 	    LWLockAcquire(&fti_state->lock, LW_EXCLUSIVE);
 	    buf = palloc(fti_state->num_elem * sizeof(uint64));
@@ -141,23 +151,16 @@ void spooler_main(Datum main_arg) {
 	}
 	if (need_clean) {
 	    for (size_t i = 0; i < size; ++i) {
-		// elog(LOG, "TransactionId from spooler_main: %lu", buf[i]);
 		fprintf(log_file, "FullTransactionId: %lu\n", buf[i]);
 	    }
 	    fflush(log_file);
 	    need_clean = false;
 	}
-	//	if (tr_id.value != old_tr_id.value)
-	//	    elog(LOG, "%lu", U64FromFullTransactionId(tr_id));
     }
 
-    //	if (U64FromFullTransactionId(2) % 10 == 2)
-    //	    ereport(NOTICE, errmsg("FulltyransactionId: %lu",
-    //				   U64FromFullTransactionId(2)));
     proc_exit(0);
 }
 
-void _PG_init(void);
 void _PG_init(void) {
     BackgroundWorker bg_worker;
     if (!process_shared_preload_libraries_in_progress) return;
@@ -167,7 +170,11 @@ void _PG_init(void) {
 	return;
     }
     DefineCustomStringVariable("tr_id_module.log_path", "log_path", NULL,
-			       &log_path, "", PGC_SIGHUP, 0, NULL, NULL, NULL);
+			       &log_path, "/home/postgres/log_tr_id_catch",
+			       PGC_SIGHUP, 0, NULL, NULL, NULL);
+    DefineCustomIntVariable("tr_id_module.spool_limit", "log_path", NULL,
+			    &spool_limit, 100, 15, 900, PGC_SIGHUP, 0, NULL,
+			    NULL, NULL);
 
     shmem_startup_hook = init_shmem;
     memset(&bg_worker, 0, sizeof(bg_worker));
@@ -183,67 +190,12 @@ void _PG_init(void) {
     bg_worker.bgw_notify_pid = 0;
     bg_worker.bgw_main_arg = (Datum)0;
 
-    //    bg_worker
     RegisterBackgroundWorker(&bg_worker);
-    //	return;
-    //    }
-    // return;
-    //  }
 
     RequestAddinShmemSpace(MAXALIGN(sizeof(FullTransactionIdSharedState)));
-    //    if (!tr_id_module_worker_start()) {
-    //	ereport(ERROR, (errcode(ERRCODE_INSUFFICIENT_RESOURCES),
-    //			errmsg("worker not REGISTER"),
-    //			errhint("Details in server log")));
-    //    return;
-    //}
-
-    //    ereport(LOG, errmsg("OK"));
-    //    if (!RegisterDynamicBackgroundWorker(&bg_worker, &handler)) {
-    //	ereport(ERROR, (errmsg("ALARM")));
-    //    }
     start_logging();
     RegisterXactCallback(FTI_inv_callback, NULL);
 }
-
-// int tr_id_module_worker_start(void) {
-//    BackgroundWorker bgw;
-//    BackgroundWorkerHandle* bgh;
-//    BgwHandleStatus status;
-//    pid_t pid;
-//
-//    elog(LOG, ("Im HEARE"));
-//    memset(&bgw, 0, sizeof(bgw));
-//
-//    bgw.bgw_flags = BGWORKER_SHMEM_ACCESS;
-//    bgw.bgw_start_time = BgWorkerStart_RecoveryFinished;
-//    snprintf(bgw.bgw_library_name, BGW_MAXLEN, "tr_id_module");
-//    snprintf(bgw.bgw_function_name, BGW_MAXLEN, "spooler_main");
-//    snprintf(bgw.bgw_name, BGW_MAXLEN, "tr_id_module worker");
-//    snprintf(bgw.bgw_type, BGW_MAXLEN, "tr_id_module");
-//
-//    bgw.bgw_restart_time = BGW_NEVER_RESTART;
-//    bgw.bgw_notify_pid = MyProcPid;
-//    bgw.bgw_main_arg = (Datum)0;
-//
-//    if (!RegisterDynamicBackgroundWorker(&bgw, &bgh)) return 1;
-//
-//    status = WaitForBackgroundWorkerStartup(bgh, &pid);
-//
-//    if (status == BGWH_STOPPED)
-//	ereport(ERROR,
-//		(errcode(ERRCODE_INSUFFICIENT_RESOURCES),
-//		 errmsg("worker not start"), errhint("Details in server log")));
-//
-//    if (status == BGWH_POSTMASTER_DIED)
-//	ereport(ERROR, (errcode(ERRCODE_INSUFFICIENT_RESOURCES),
-//			errmsg("worker not start without postmaster"),
-//			errhint("Kill all proc and restart DB")));
-//
-//    Assert(status == BGWH_STARTED);
-//
-//    return 1;
-//}
 
 static void tim_sigterm_hadler(SIGNAL_ARGS) {
     int save_errno = errno;
@@ -265,11 +217,3 @@ static void tim_sighup_hadler(SIGNAL_ARGS) {
     errno = save_errno;
 }
 
-// void _PG_fini(void);
-// void _PG_fini(void) {
-//    //   stop_logging();
-//
-//    TerminateBackgroundWorker(handler);
-//    UnregisterXactCallback(FTI_inv_callback, NULL);
-//}
-//
